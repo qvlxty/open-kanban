2abe8994622fd293e4f3a44990c338b210822752
qvlxty <qualixity@gmail.com>
2025-10-23 01:33:00 +0700

    fix: remove unused styles

--- frontend/src/shared/ui/theming/global.styled.tsx
+++ frontend/src/shared/ui/theming/global.styled.tsx
@@ -1,8 +1,5 @@
 import { createGlobalStyle } from 'styled-components'
 
-// import RobotoFont from '../fonts/Roboto.ttf'
-// import RobotoBoldFont from '../fonts/Roboto-Bold.ttf'
-// import RobotoBlackFont from '../fonts/Roboto-Black.ttf'
 import { themeVar } from './helpers'
 import { ThemedStyledProps } from './types'
 
@@ -24,7 +21,6 @@ export const GlobalStyled = createGlobalStyle<ThemedStyledProps>`
 
     a {
         text-decoration: none;
-        color: #111;
     }
 
     label {
@@ -64,47 +60,9 @@ export const GlobalStyled = createGlobalStyle<ThemedStyledProps>`
     }
 
     h1,h2,h3 {
-    padding: 0;
-    margin: 0;
+        padding: 0;
+        margin: 0;
     }
 
-
-    .ScrollbarsCustom-TrackY {
-        width: 4px !important;
-    }
-
-    kbd {
-    background: #000;
-    border: 1px solid #000;
-    font-family: Roboto;
-    font-weight: 500;
-    color: #fff;
-    font-size: 14px;
-    margin-left: 8px;
-    padding: 2px;
-    padding-left: 5px;
-    padding-right: 5px;
-    border-radius: 4px;
-    }
-    .react-flow__handle {
-    width: 0;
-    height: 0;
-    } 
-
-    code {
-        font-family: 'Roboto';
-    }
-
-    .copied {
-        svg {
-            fill: white;
-        }
-    }
-
-    .wmde-markdown  {
-        font-family: 'Roboto';
-        background: none;
-        color: ${themeVar('fontColor')};
-    }
 
 `
